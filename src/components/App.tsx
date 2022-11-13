1b9234baff7a9fc97a1b68eee8fbb8ede9eeb502
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-13 18:39:02 +0300

    updated App component and CSS styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+import { ClientType } from "../types/ClientType";
+
+import Button from "./Button";
 import Clients from "./Clients";
 import Criteria from "./Criteria";
 import Products from "./Products";
 
 const App = () => {
+  const [clients, setClients] = useState<ClientType[]>([
+    {
+      id: 1,
+      name: "Clockwork Inc.",
+    },
+    {
+      id: 2,
+      name: "Macrosoft",
+    },
+    {
+      id: 3,
+      name: "Bodybook",
+    },
+  ]);
+
   return (
-    <>
-      <Clients />
+    <div className="container">
+      <div className="clients">
+        {clients.length > 0 ? (
+          <Clients clients={clients} />
+        ) : (
+          <p style={{ marginLeft: "10px" }}>No clients to show!</p>
+        )}
+        <Button
+          color="#4A86E8"
+          text="Add a new client"
+          onClick={() => console.log("Button pressed")}
+        />
+      </div>
       <Criteria />
       <Products />
-    </>
+    </div>
   );
 };
 
