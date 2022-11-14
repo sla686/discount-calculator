4ca2126f5d54c1b9caf729f85f9401324f655e72
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-14 03:14:31 +0300

    created InputProduct component

---
+++ src/components/InputProduct.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, useState } from "react";
+
+const InputProduct = ({
+  addProduct,
+}: {
+  addProduct: (name: string, price: string) => void;
+}) => {
+  const [inputName, setInputName] = useState("");
+  const [inputPrice, setInputPrice] = useState("");
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputPrice && inputName.trim() && inputPrice.trim()) {
+      addProduct(inputName, inputPrice);
+      setInputName("");
+      setInputPrice("");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add product..."
+        value={inputName}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInputName(e.target.value)
+        }
+        name="name"
+      />
+      <input
+        type="number"
+        className="input-text"
+        placeholder="Add price..."
+        value={inputPrice}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInputPrice(e.target.value)
+        }
+        name="price"
+      />
+      <input type="submit" className="btn btn-block" value={"Add new client"} />
+    </form>
+  );
+};
+
+export default InputProduct;
