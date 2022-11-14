7bba2f319d8f6d35d3f62c8831105f2fcee823c6
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-14 03:15:27 +0300

    implemented Client and its logic

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,14 +1,24 @@
 import { ClientType } from "../types/ClientType";
 import Client from "./Client";
+import InputClient from "./InputClient";
 
-const Clients = ({ clients }: { clients: ClientType[] }) => {
+const Clients = ({
+  clients,
+  addClient,
+}: {
+  clients: ClientType[];
+  addClient: (name: string) => void;
+}) => {
   return (
-    <>
+    <div className="clients">
       <h2>Clients</h2>
-      {clients.map((client) => (
-        <Client key={client.id} client={client} />
-      ))}
-    </>
+      <InputClient addClient={addClient} />
+      {clients.length > 0 ? (
+        clients.map((client) => <Client key={client.id} client={client} />)
+      ) : (
+        <p style={{ marginLeft: "10px" }}>No clients to show!</p>
+      )}
+    </div>
   );
 };
 
