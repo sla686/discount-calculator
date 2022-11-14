9a157167949d47218cdb8f88931c69b9f160015a
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-14 03:15:21 +0300

    implemented Product and its logic

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,27 @@
-const Products = () => {
-  return <div className="products">Products</div>;
+import { ProductType } from "../types/ProductType";
+import InputProduct from "./InputProduct";
+import Product from "./Product";
+
+const Products = ({
+  products,
+  addProduct,
+}: {
+  products: ProductType[];
+  addProduct: (name: string, price: string) => void;
+}) => {
+  return (
+    <div className="products">
+      <h2>Products</h2>
+      <InputProduct addProduct={addProduct} />
+      {products.length > 0 ? (
+        products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
+      ) : (
+        <p style={{ marginLeft: "10px" }}>No products to show!</p>
+      )}
+    </div>
+  );
 };
 
 export default Products;
