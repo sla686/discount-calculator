4e64d555931194281580a51cb75e6dfcff2b0d29
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-14 13:05:17 +0300

    implemented deletion on each item

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,10 +1,21 @@
 import { ClientType } from "../types/ClientType";
+import { FaTimesCircle } from "react-icons/fa";
 
-const Client = ({ client }: { client: ClientType }) => {
+const Client = ({
+  client,
+  delClient,
+}: {
+  client: ClientType;
+  delClient: (id: string, type: string) => void;
+}) => {
   return (
     <>
       <div className="client">
         <h3>{client.name}</h3>
+        <FaTimesCircle
+          className="circle"
+          onClick={() => delClient(client.id, "client")}
+        />
       </div>
     </>
   );
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,22 @@
 import { ProductType } from "../types/ProductType";
+import { FaTimesCircle } from "react-icons/fa";
 
-const Product = ({ product }: { product: ProductType }) => {
+const Product = ({
+  product,
+  delProduct,
+}: {
+  product: ProductType;
+  delProduct: (id: string, type: string) => void;
+}) => {
   return (
     <>
       <div className="product">
         <h3>{product.name}</h3>
         <p>{product.price}$</p>
+        <FaTimesCircle
+          className="circle"
+          onClick={() => delProduct(product.id, "product")}
+        />
       </div>
     </>
   );
